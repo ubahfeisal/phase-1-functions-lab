e9040ef4d70750c2f32949ca581e88de6ea7edda
ubah feisal <feisalubah1@gmail.com>
2023-06-13 15:09:56 +0300

    done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(distance) {
+    const blocks = distanceFromHqInBlocks(distance);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(destination - start);
+    return distance * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
